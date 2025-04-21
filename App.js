e3ac89db6a3d8add0b35c04a7e23238a279caa7a
Liam Kelso <liamkelso02@gmail.com>
2025-04-21 14:45:42 -0500

    app.js changes

--- App.js
+++ App.js
@@ -4,52 +4,76 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// Import screen components
-import WelcomeScreen from './screens/WelcomeScreen';
-import LoginScreen from './screens/LoginScreen';
-import GroupScreen from './screens/GroupScreen';
-import EventScreen from './screens/EventScreen';
-import AddSliceScreen from './screens/AddSliceScreen';
-import DayCalendar from './screens/DayCalendar';
-import Event from './screens/Event';
-import WheelOfFortune from './screens/WheelOfFortune';
-import EventsOfWeek from './screens/EventsOfWeek';
-import EventCard from './screens/EventCard';
+// Import your screen components
+import WelcomeScreen   from './screens/WelcomeScreen';
+import LoginScreen     from './screens/LoginScreen';
+import InputSchedule   from './screens/InputSchedule';   // added
+import GroupScreen     from './screens/GroupScreen';
+import EventScreen     from './screens/EventScreen';
+import EventsOfWeek    from './screens/EventsOfWeek';
+import EventCard       from './screens/EventCard';
+import DayCalendar     from './screens/DayCalendar';
+import Event           from './screens/Event';
+import WheelOfFortune  from './screens/WheelOfFortune';
 
-// Stack navigator setup
 const Stack = createStackNavigator();
 
-const App = () => {
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="LoginScreen">
-        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="LoginScreen" component={LoginScreen}  
-        options={{
-          headerShown: false,
-        }} />
-        <Stack.Screen name="GroupScreen" component={GroupScreen}
-        options={{
-          headerShown: false, 
-        }} />
-        <Stack.Screen name="EventScreen" component={EventScreen} />
-        <Stack.Screen name="EventsOfWeek" component={EventsOfWeek}
-        options={{
-          headerShown: false, 
-        }}
+        {/* Welcome / Auth flow */}
+        <Stack.Screen 
+          name="WelcomeScreen" 
+          component={WelcomeScreen} 
+          options={{ headerShown: false }} 
         />
-        <Stack.Screen name="EventCard" component={EventCard}
-        options={{
-          headerShown: false, 
-        }}
+        <Stack.Screen 
+          name="LoginScreen" 
+          component={LoginScreen} 
+          options={{ headerShown: false }} 
         />
-        <Stack.Screen name="AddSliceScreen" component={AddSliceScreen} />
-        <Stack.Screen name="DayCalendar" component={DayCalendar} />
-        <Stack.Screen name="Event" component={Event} />
-        <Stack.Screen name="WheelOfFortune" component={WheelOfFortune} />
+
+        {/* Busy‑time entry / editing */}
+        <Stack.Screen 
+          name="InputSchedule" 
+          component={InputSchedule} 
+          options={{ headerShown: false }} 
+        />
+
+        {/* Main app */}
+        <Stack.Screen 
+          name="GroupScreen" 
+          component={GroupScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="EventScreen" 
+          component={EventScreen} 
+        />
+        <Stack.Screen 
+          name="EventsOfWeek" 
+          component={EventsOfWeek} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="EventCard" 
+          component={EventCard} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="DayCalendar" 
+          component={DayCalendar} 
+        />
+        <Stack.Screen 
+          name="Event" 
+          component={Event} 
+        />
+        <Stack.Screen 
+          name="WheelOfFortune" 
+          component={WheelOfFortune} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
-
-export default App;
+}
